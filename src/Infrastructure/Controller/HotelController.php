c3e998604166202825b57397137932aa80906432
Sergio Yamuza <syamuza@freepik.com>
2021-06-22 19:01:00 +0200

    Install PHP mysql pdo exntension extension and set up database connection through simple PDO class.
    Define THNPDO service through dependency inyection with autowiring

--- src/Infrastructure/Controller/HotelController.php
+++ src/Infrastructure/Controller/HotelController.php
@@ -2,14 +2,26 @@
 
 namespace THN\Infrastructure\Controller;
 
+use PDO;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use THN\Infrastructure\Persistence\THNPDO;
 
 class HotelController extends AbstractController
 {
+    private $pdo;
+
+    public function __construct(THNPDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
 
     public function list(): Response
     {
-        return $this->render('base.html.twig');
+        $hotels = $this->pdo->query("SELECT * FROM `hotel`");
+
+        return $this->render('base.html.twig', [
+            'hotels' => $hotels
+        ]);
     }
 }
